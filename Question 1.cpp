bdc7f5279af7e5d762c9ba4c131a483a97392f72
niharikachawla24 <nchawla_be24@thapar.edu>
2025-11-06 15:20:13 +0530

    Create Question 1.cpp

---
+++ Question 1.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+int size=50;
+int queue[50];
+int front= -1; 
+int rear=-1;
+void enqueue ();
+int isFull();
+int isEmpty();
+void dequeue();
+void display();
+void peek();
+
+int main() {
+    int input;  
+    while (1) { 
+        std::cout << "Press:\n 1:Add \n 2:Remove\n 3:check if empty \n 4:check if full \n 5: display elements\n 6: use peek function \n 7 to exit\n";
+        std::cin >> input;
+        std::cout << "\n";
+             switch(input) {
+            case 1: enqueue();
+            break;
+            case 2: dequeue();
+            break;
+            case 3: isEmpty();
+            break;
+            case 4: isFull();
+            break;
+            case 5: display();
+            break;
+             case 6: peek();
+            break;
+        }
+        if(input == 7) break;
+    }
+    return 0;
+
+} 
+
+int isFull(){
+    if(rear==size-1){ std::cout<<"Overflow\n" ; return 1;}else return 0;
+}
+int isEmpty(){
+    if(rear==-1&&front==-1){ std::cout<<"Underflow\n" ; return 1;}else return 0;
+}
+void enqueue(){ int data;
+    if(isFull()==0 ){ if(front==-1){front=0;}
+    rear=rear+1; 
+       std::cout<<"Enter:"; std::cin>>data;
+         queue[rear]=data; 
+        
+    } 
+}
+
+void dequeue(){if(isEmpty()==0){int data=queue[front];
+     if(front==rear){
+            front=rear=-1;}else front=front+1; 
+       std::cout<<"Deleted:"<<data<< "\n";
+}
+}
+
+void display() {
+    if (isEmpty() == 1) {
+        std::cout << "Queue is empty\n";
+    } else {
+        std::cout << "Elements: ";
+        for (int i=front; i <=rear; i++) {
+            std::cout << queue[i] << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+    void peek() {
+    if (isEmpty() == 1) {
+        std::cout << "Queue is empty\n";
+    } else {
+        std::cout << "First element: " << queue[front] << "\n";
+    }
+}
