ea95fd37d006f3cec15a50f1da930bd888608738
niharikachawla24 <nchawla_be24@thapar.edu>
2025-11-10 06:23:10 +0530

    Update Question 3.cpp

--- Question 3.cpp
+++ Question 3.cpp
@@ -1,37 +1,36 @@
-#include<iostream>
-#include<queue>
-int main(){
+#include <iostream>
+#include <queue>
+using namespace std;
 
-std::queue<int> queue1;
-std::queue<int> queue2;
-for(int i=0;i<6;i++)
-{
- int x=i+1;
- queue1.push(x);
-}
+int main() {
+    queue<int> queue1;
+    queue<int> queue2;
 
-int i=0;
-while(i<3)
-{
- int x=queue1.front();
- queue1.pop();
- queue2.push(x);
- i++;
-}
+    for (int i = 0; i < 6; i++) {
+        int x = i + 1;
+        queue1.push(x);
+    }
 
-while(!queue2.empty())
-{
- int z=queue1.front();
- queue1.pop();
- queue1.push(z);
- int q=queue2.front();
- queue2.pop();
- queue1.push(q);
-}
+    int n = queue1.size();
+    for (int i = 0; i < n / 2; i++) {
+        queue2.push(queue1.front());
+        queue1.pop();
+    }
 
-while(!queue1.empty())
-{
- std::cout<<queue1.front()<<"\t";
- queue1.pop();
-}
+    while (!queue1.empty() && !queue2.empty()) {
+        int q = queue2.front();
+        queue2.pop();
+        queue1.push(q);
+
+        int z = queue1.front();
+        queue1.pop();
+        queue1.push(z);
+    }
+
+    while (!queue1.empty()) {
+        cout << queue1.front() << "\t";
+        queue1.pop();
+    }
+
+ return 0;
 }
