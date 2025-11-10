06c9d953f17e9e172974e24e115369bfd6769bc8
niharikachawla24 <nchawla_be24@thapar.edu>
2025-11-10 06:29:09 +0530

    Update Question 4.cpp

--- Question 4.cpp
+++ Question 4.cpp
@@ -1,74 +1,79 @@
 #include <iostream>
-class Queue1{
-    public: char Queue[50]; 
-    int front=-1; 
-    int rear=-1;
-    int size=50;
-   int isEmpty(){
-       if((front==-1&&rear==-1)||front>rear){
-           return 1;  
-     }
-       else
-           return 0;
-}
-  int  isFull(){
-      if(rear==size-1){
-          return 1;
-      }
-    else
-          return 0;
-}
-    char peek(){
-        if(!isEmpty()){
+using namespace std;
+
+class Queue1 {
+public:
+    char Queue[50];
+    int front = -1;
+    int rear = -1;
+    int size = 50;
+
+    int isEmpty() {
+        if ((front == -1 && rear == -1) || front > rear)
+            return 1;
+        else
+            return 0;
+    }
+
+    int isFull() {
+        if (rear == size - 1)
+            return 1;
+        else
+            return 0;
+    }
+
+    char peek() {
+        if (!isEmpty())
             return Queue[front];
-        }
         else
             return '\0';
     }
-    
-   void enqueue(char x){
-       if(isFull()){
-           std::cout<<"Queue full!";
-       }
-       else if(front==-1&&rear==-1){
-               front=0; 
-               rear=0;
-               {
-       else {
-               rear=rear+1;}
-        Queue[rear]=x; 
-   
+
+    void enqueue(char x) {
+        if (isFull()) {
+            cout << "Queue full!\n";
+        } else if (front == -1 && rear == -1) {
+            front = rear = 0;
+            Queue[rear] = x;
+        } else {
+            rear = rear + 1;
+            Queue[rear] = x;
+        }
     }
-    char dequeue(){
-        if(isEmpty()){
-            std::cout<<"Empty queue!";
+
+    char dequeue() {
+        if (isEmpty()) {
+            cout << "Empty queue!\n";
             return '\0';
-        } 
-    else{
-        char z= Queue[front];
-        front=front+1;
-        return z;
+        } else {
+            char z = Queue[front];
+            front = front + 1;
+            return z;
+        }
     }
-      }
 };
-void NonRepeating(char Array[],int array_size){
+
+void NonRepeating(char Array[], int array_size) {
     Queue1 q1;
-   int freq[256]={0};
-   
-   for(int i=0;i<array_size;i++)
-   {     q1.enqueue(Array[i]);
-        freq[Array[i]]++;
-    
-     while(!q1.isEmpty() && freq[q1.peek()] >1)
-    {      q1.dequeue();
-        }  if(q1.isEmpty()){ std::cout<<"-1\n";}
-       else std::cout<<q1.peek()<<std::endl;
-        
-     } 
-    
+    int freq[256] = {0};
+
+    for (int i = 0; i < array_size; i++) {
+        q1.enqueue(Array[i]);
+        freq[(unsigned char)Array[i]]++;
+
+        while (!q1.isEmpty() && freq[(unsigned char)q1.peek()] > 1) {
+            q1.dequeue();
+        }
+
+        if (q1.isEmpty())
+            cout << "-1\n";
+        else
+            cout << q1.peek() << endl;
+    }
 }
 
-int main() { char Array[5]={'a','a','b','b','d'};
-    
-NonRepeating(Array,5);
+int main() {
+    char Array[5] = {'a', 'a', 'b', 'b', 'd'};
+    NonRepeating(Array, 5);
 }
+
